642fe7bd30af7ca927bab70ee5cf412986755132
cbrzn <cesarbrazon10@gmail.com>
2022-05-18 14:05:49 +0200

    chore(js-vanilla-hello-world): remove css plugin, add manifest.json & readme

---
+++ hello-world/app/js-vanilla/README.md
@@ -0,0 +1,26 @@
+## Available Scripts
+
+In the project directory, you can run:
+
+### `nvm install && nvm use`
+
+Install and use the proper version of node.
+
+### `yarn`
+
+Install all dependencies.
+
+### `yarn build`
+
+Build and bundle code
+
+### `yarn start`
+
+Runs the app.<br />
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+If you make edits, you will need to run `yarn build && yarn start` to see the changes<br />
+
+## Learn More
+
+To learn more about Polywrap, visit our [documentation page](https://docs.polywrap.io/).
--- hello-world/app/js-vanilla/src/index.js
+++ hello-world/app/js-vanilla/src/index.js
@@ -1,24 +1,28 @@
-import { HelloWorld_Query } from './w3' 
-import { Web3ApiClient } from "@web3api/client-js"
-import Toastify from 'toastify-js'
-import "toastify-js/src/toastify.css"
+import { HelloWorld_Query } from "./w3";
+import { Web3ApiClient } from "@web3api/client-js";
+import Toastify from "toastify-js";
 
 function invokeClient() {
-  const client = new Web3ApiClient()
-  const message = document.getElementById("message_input").value
-  const result = HelloWorld_Query.logMessage({ message }, client)
-  result.then(result => console.log({ result })).catch(error => console.log({ error }))
+  const client = new Web3ApiClient();
+  const message = document.getElementById("message_input").value;
+  const result = HelloWorld_Query.logMessage({ message }, client);
+  console.info("Invoking Method: logMessage");
+  result
+    .then((result) =>
+      console.info(`Invoke Result: ${JSON.stringify(result, null, 2)}`)
+    )
+    .catch((error) => console.log({ error }));
 }
 
-document.getElementById('invoke_button').addEventListener('click', function () {
+document.getElementById("invoke_button").addEventListener("click", function () {
   Toastify({
     text: "Take a look at your console!",
     style: {
       fontSize: "20px",
       fontWeight: "500",
       color: "white",
-      background: "#60c093"
-    }
-  }).showToast()
-  invokeClient()
-})
+      background: "#60c093",
+    },
+  }).showToast();
+  invokeClient();
+});
