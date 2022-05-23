df85cb8140e53784f26a3998b040178814ef624a
dOrgJelli <jelli@dorg.tech>
2022-05-22 23:55:09 -0400

    add example URI to UI

--- fetch-metadata/src/pages/Main.tsx
+++ fetch-metadata/src/pages/Main.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import {Grid, styled} from "@mui/material";
+import {Grid, styled, Typography} from "@mui/material";
 import {FetchMetadata} from "../components/FetchMetadata/FetchMetadata";
 import {DisplayMetadata} from "../components/DisplayMetadata/DisplayMetadata";
 import {MetaManifest} from "@web3api/client-js";
@@ -20,7 +20,12 @@ export const Main: React.FC = () => {
         <FetchMetadata setManifest={setManifest} setIcons={setIcons} />
       </Grid>
       <Grid item container xs={12} lg={6} direction="row" justifyContent="center" spacing={0}>
-        {manifest && <DisplayMetadata manifest={manifest} icons={icons} />}
+        {manifest ?
+          <DisplayMetadata manifest={manifest} icons={icons} /> :
+          <Typography variant={"subtitle2"}>
+            <strong>Example: w3://ipfs/QmcFf5GY1EboKGtf1cJQM8BxyPbwk3pVjwt4zVyiYeftSD</strong>
+          </Typography>
+        }
       </Grid>
     </AppContainer>
   );
