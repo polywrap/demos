1c93b78cb51c65e39b3c32bc45f3170cc5926c6d
cbrzn <cesarbrazon10@gmail.com>
2022-06-24 20:50:34 +0200

    chore: hello world wrapper finished

--- hello-world/wrapper/assemblyscript/src/__tests__/e2e.spec.ts
+++ hello-world/wrapper/assemblyscript/src/__tests__/e2e.spec.ts
@@ -1,11 +1,18 @@
 import { PolywrapClient } from "@polywrap/client-js";
 
+jest.setTimeout(30000)
+
 describe("Hello world wrapper", () => {
   const client = new PolywrapClient()
   it("Should log as warning", async () => {
-    await client.invoke({
+
+    const invocation = await client.invoke({
       uri: "fs/./build",
-      method: "warning",
+      method: "logMessage",
+      input: {
+        message: "Invokation being executed from wrapper!"
+      }
     })
+    expect(invocation).toBeTruthy()
   });
 })
