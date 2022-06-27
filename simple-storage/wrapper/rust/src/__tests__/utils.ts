1adcf4322ef33aa4601e5cba4b33ad33888daf51
Niraj Kamdar <nirajkamdar27@pm.me>
2022-06-27 17:00:37 +0400

    feat(simple-storage): add necessary files to rust impl

--- simple-storage/wrapper/rust/src/__tests__/utils.ts
+++ simple-storage/wrapper/rust/src/__tests__/utils.ts
@@ -1,30 +1,12 @@
-import { PolywrapClientConfig } from "@polywrap/client-js";
-import { ClientConfig, coreInterfaceUris } from "@polywrap/client-js";
+import { ClientConfig } from "@polywrap/client-js";
 import { ensPlugin } from "@polywrap/ens-plugin-js";
 import { ethereumPlugin } from "@polywrap/ethereum-plugin-js";
 import { ipfsPlugin } from "@polywrap/ipfs-plugin-js";
 
-interface TestEnvironment {
-  ipfs: string;
-  ethereum: string;
-  ensAddress: string;
-  registrarAddress?: string;
-  reverseAddress?: string;
-  resolverAddress?: string;
-}
-
-async function getProviders(): Promise<TestEnvironment> {
-  const ipfs = "http://localhost:5001";
-  const ethereum = "http://localhost:8545";
-  const ensAddress = "0xe78A0F7E598Cc8b0Bb87894B0F60dD2a88d6a8Ab";
-
-  return { ipfs, ethereum, ensAddress };
-}
-
-function getPlugins(
+export function getPlugins(
   ethereum: string,
   ipfs: string,
-  ensAddress: string
+  ensAddress: string,
 ): Partial<ClientConfig> {
   return {
     redirects: [],
@@ -51,14 +33,6 @@ function getPlugins(
           defaultNetwork: "testnet",
         }),
       },
-    ]
+    ],
   };
-}
-
-export async function getClientConfig(
-  _: Partial<PolywrapClientConfig>
-): Promise<Partial<PolywrapClientConfig>> {
-  const { ipfs, ethereum, ensAddress } = await getProviders();
-
-  return getPlugins(ethereum, ipfs, ensAddress);
 }
