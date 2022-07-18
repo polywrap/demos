248485a7a7d935f1bbcd7c549f376ac24f26fdab
krisbitney <kristoferbitney@hotmail.com>
2022-07-18 13:16:29 +0300

    removed old datetime plugin

--- old/datetime-plugin/plugin/src/resolvers.ts
+++
@@ -1,8 +0,0 @@
-import { DateTimePlugin } from ".";
-import { Query } from "./w3";
-
-export const query = (plugin: DateTimePlugin): Query.Module => ({
-  currentTime: () => {
-    return plugin.currentTime();
-  },
-});
