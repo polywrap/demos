e2f01348abd3ef74c4b6cc3368ab6738dfb06d9c
cbrzn <cesarbrazon10@gmail.com>
2022-07-22 00:06:47 +0200

    feat(signer-provider): app created

---
+++ signer-provider/src/index.ts
@@ -0,0 +1,93 @@
+import { Ethereum_Module } from "./wrap";
+import { PolywrapClient } from "@polywrap/client-js";
+import Toastify from "toastify-js";
+import { ethereumPlugin } from "@polywrap/ethereum-plugin-js";
+import { ethers } from 'ethers';
+import { JsonRpcProvider } from "@polywrap/client-js/build/pluginConfigs/Ethereum";
+
+declare let window: {
+  ethereum?: {
+    chainId: string
+    selectedAddress?: string
+  }
+}
+
+const KNOWN_NETWORKS = {
+  1: "mainnet"
+}
+
+let client;
+
+const authenticate = async () => {
+  if (!window.ethereum) {
+    Toastify({
+      text: "You must install MetaMask!",
+      style: {
+        fontSize: "20px",
+        fontWeight: "500",
+        color: "white",
+        background: "#60c093",
+      },
+    }).showToast();
+    return
+  }
+
+  const provider = new ethers.providers.Web3Provider(window.ethereum as JsonRpcProvider);
+  await provider.send('eth_requestAccounts', [])
+  const signer = provider.getSigner()
+
+  const chainId = Number(window.ethereum.chainId)
+  const chainName = KNOWN_NETWORKS[chainId] 
+  const config = {
+    networks: {
+      [chainName]: {
+        provider,
+        signer
+      }
+    },
+    defaultNetwork: "mainnet"
+  }
+  console.log(config)
+
+  client = new PolywrapClient({
+    plugins: [
+      {
+        uri: "wrap://ens/ethereum.polywrap.eth",
+        plugin: ethereumPlugin(config)
+      }
+    ]
+  })
+}
+
+const invokeClient = async () => {
+  try {
+    if (client) {
+      console.log(client)
+      console.info("Invoking Method: Ethereum_Module.signMessage");
+      const result = await Ethereum_Module.signMessage({ message: "Signing message from polywrap" }, client);
+      console.info(`Invoke Result: ${JSON.stringify(result, null, 2)}`);
+    }
+    await authenticate()   
+  } catch (error) {
+    console.log({ error });
+  }
+};
+
+const executeInvoke = async () => {
+  // Toastify({
+  //   text: "Take a look at your console!",
+  //   style: {
+  //     fontSize: "20px",
+  //     fontWeight: "500",
+  //     color: "white",
+  //     background: "#60c093",
+  //   },
+  // }).showToast();
+
+  await invokeClient();
+};
+
+const button: HTMLButtonElement = document.getElementById(
+  "invoke_button"
+) as HTMLButtonElement;
+button?.addEventListener("click", invokeClient);
