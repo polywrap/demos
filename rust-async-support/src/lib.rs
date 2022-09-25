5749b1d7dc4e378f650abc19cdf4a4846e78ebf6
dOrgJelli <jelli@dorg.tech>
2022-09-25 15:50:55 +0200

    clean up demo

--- rust-async-support/src/lib.rs
+++ rust-async-support/src/lib.rs
@@ -1,23 +1,18 @@
 use futures::executor;
-use std::future::Future;
 pub mod wrap;
 pub use wrap::*;
 use wrap::{ArgsHelloWorld, ArgsHelloWorldSpawnLocal};
+
+async fn concat_str(message: String) -> String {
+    format!("{} {}", message, "foo bar")
+}
 
 pub fn hello_world(args: ArgsHelloWorld) -> String {
     args.message
 }
 
-// https://docs.rs/wasm-bindgen-futures/latest/wasm_bindgen_futures/fn.spawn_local.html
-// Spawns a Future<Output = ()> on the current thread.
-// the Future is executed in the background on the next microtask tick.
 pub fn hello_world_spawn_local(args: ArgsHelloWorldSpawnLocal) -> String {
-    let message_copy = args.message.clone();
-
-    // use `async move` to force the async block to take ownership of variables
-    let result = executor::block_on(async move {
-        format!("{} {}", message_copy, "foo bar")
-    });
+    let result = executor::block_on(concat_str(args.message));
 
     result
 }
