9babacb11939c728f7812115450897c4e8646294
krisbitney <kristoferbitney@hotmail.com>
2023-04-15 12:02:11 +0000

    updated common-interface demo to 0.10.0

--- common-interface/wrapper/assemblyscript/src/index.ts
+++ common-interface/wrapper/assemblyscript/src/index.ts
@@ -1,16 +1,19 @@
 import {
   Args_abstractMethod,
-  Common_Object
+  Common_Object, ModuleBase
 } from "./wrap";
 
-export function abstractMethod(
-  args: Args_abstractMethod
-): Common_Object {
-  return {
-    prop1: args.arg1,
-    prop2: args.arg2,
-    prop3: {
-      prop: args.arg1 + args.arg2.toString()
-    }
-  };
+export class Module extends ModuleBase {
+
+  abstractMethod(
+    args: Args_abstractMethod
+  ): Common_Object {
+    return {
+      prop1: args.arg1,
+      prop2: args.arg2,
+      prop3: {
+        prop: args.arg1 + args.arg2.toString()
+      }
+    };
+  }
 }
