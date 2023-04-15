ac81feda2b1fc71483708023c747412e01ef18f4
krisbitney <kristoferbitney@hotmail.com>
2023-04-15 13:11:51 +0000

    updated simple-storage assemblyscript demo wrapper to 0.10.0

--- simple-storage/wrapper/assemblyscript/src/__tests__/utils.ts
+++ simple-storage/wrapper/assemblyscript/src/__tests__/utils.ts
@@ -1,35 +1,41 @@
-import { ClientConfig } from "@polywrap/client-js";
-import { ensResolverPlugin } from "@polywrap/ens-resolver-plugin-js";
-import { ethereumPlugin } from "@polywrap/ethereum-plugin-js";
-import { ipfsPlugin } from "@polywrap/ipfs-plugin-js";
+import { ethereumProviderPlugin, ProviderConfig, Connections, Connection } from "@polywrap/ethereum-provider-js";
+import { ClientConfigBuilder, DefaultBundle } from '@polywrap/client-config-builder-js';
+import {CoreClientConfig, ExtendableUriResolver, IWrapPackage} from "@polywrap/client-js";
+import {ETH_ENS_IPFS_MODULE_CONSTANTS} from "polywrap";
 
-export function getPlugins(
-  ethereum: string,
-  ipfs: string,
-  ensAddress: string,
-): Partial<ClientConfig> {
-  return {
-    redirects: [],
-    plugins: [
-      {
-        uri: "wrap://ens/ipfs.polywrap.eth",
-        plugin: ipfsPlugin({ provider: ipfs }),
-      },
-      {
-        uri: "wrap://ens/ens.polywrap.eth",
-        plugin: ensResolverPlugin({ addresses: { testnet: ensAddress } }),
-      },
-      {
-        uri: "wrap://ens/ethereum.polywrap.eth",
-        plugin: ethereumPlugin({
-          networks: {
-            testnet: {
-              provider: ethereum,
-            },
-          },
-          defaultNetwork: "testnet",
+export function getConfig(): CoreClientConfig {
+  const ethereumConfig: ProviderConfig = {
+    connections: new Connections({
+      networks: {
+        testnet: new Connection({
+          provider: ETH_ENS_IPFS_MODULE_CONSTANTS.ethereumProvider // Ganache test network,
         }),
       },
-    ],
+      defaultNetwork: "testnet",
+    }),
   };
+
+  return new ClientConfigBuilder()
+    .addDefaults()
+    .addPackage(
+      DefaultBundle.plugins.ethereumProviderV2.uri.uri,
+      ethereumProviderPlugin(ethereumConfig) as IWrapPackage
+    )
+    .addEnv(DefaultBundle.embeds.ipfsResolver.source.uri, {
+      provider: ETH_ENS_IPFS_MODULE_CONSTANTS.ipfsProvider,
+      fallbackProviders: DefaultBundle.ipfsProviders,
+      retries: { tryResolveUri: 2, getFile: 2 },
+    })
+    .addEnv("proxy/testnet-ens-uri-resolver-ext", {
+      registryAddress: ETH_ENS_IPFS_MODULE_CONSTANTS.ensAddresses.ensAddress,
+    })
+    .addRedirect(
+      "proxy/testnet-ens-uri-resolver-ext",
+      "ens/wraps.eth:ens-uri-resolver-ext@1.0.1"
+    )
+    .addInterfaceImplementation(
+      ExtendableUriResolver.defaultExtInterfaceUris[0].uri,
+      "proxy/testnet-ens-uri-resolver-ext"
+    )
+    .build();
 }
--- simple-storage/wrapper/assemblyscript/src/index.ts
+++ simple-storage/wrapper/assemblyscript/src/index.ts
@@ -3,37 +3,42 @@ import {
   Args_deployContract,
   Args_setData,
   Args_getData,
+  ModuleBase,
 } from "./wrap";
 import { abi, bytecode } from "./contracts/SimpleStorage";
 
-export function getData(args: Args_getData): u32 {
-  const res = Ethereum_Module.callContractView({
-    address: args.address,
-    method: "function get() view returns (uint256)",
-    args: null,
-    connection: args.connection,
-  }).unwrap();
+export class Module extends ModuleBase {
 
-  return U32.parseInt(res);
-}
+  getData(args: Args_getData): u32 {
+    const res = Ethereum_Module.callContractView({
+      address: args.address,
+      method: "function get() view returns (uint256)",
+      args: null,
+      connection: args.connection,
+    }).unwrap();
 
-export function setData(args: Args_setData): string {
-  const res = Ethereum_Module.callContractMethod({
-    address: args.address,
-    method: "function set(uint256 value)",
-    args: [args.value.toString()],
-    connection: args.connection,
-    txOverrides: null,
-  }).unwrap();
+    return U32.parseInt(res);
+  }
 
-  return res.hash;
-}
+  setData(args: Args_setData): string {
+    const res = Ethereum_Module.callContractMethod({
+      address: args.address,
+      method: "function set(uint256 value)",
+      args: [args.value.toString()],
+      connection: args.connection,
+      options: null,
+    }).unwrap();
 
-export function deployContract(args: Args_deployContract): string {
-  return Ethereum_Module.deployContract({
-    abi,
-    bytecode,
-    args: null,
-    connection: args.connection,
-  }).unwrap();
+    return res.hash;
+  }
+
+  deployContract(args: Args_deployContract): string {
+    return Ethereum_Module.deployContract({
+      abi,
+      bytecode,
+      args: null,
+      connection: args.connection,
+      options: null,
+    }).unwrap();
+  }
 }
